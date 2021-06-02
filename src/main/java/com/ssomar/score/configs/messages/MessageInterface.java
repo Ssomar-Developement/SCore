43ec8f4616c82cea377e942a8376ffb05359e1b2
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-06-02 13:53:01 +0200

    update MessageInterface

--- src/main/java/com/ssomar/score/configs/messages/MessageInterface.java
+++ src/main/java/com/ssomar/score/configs/messages/MessageInterface.java
@@ -1,10 +1,17 @@
 package com.ssomar.score.configs.messages;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface MessageInterface {
 
 	public String getName();
 	
-	public List<MessageInterface> getMessagesEnum();
+	public static List<MessageInterface> getMessagesEnum(MessageInterface[] tab) {
+		List<MessageInterface> result = new ArrayList<>();
+		for(MessageInterface msg : tab){
+			result.add(msg);
+		}
+		return result;
+	}
 }
