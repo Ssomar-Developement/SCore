07c6e547b3760be919eec05291dc3d297ab69bcb
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-06-14 12:55:39 +0200

    add customcolor util

---
+++ src/main/java/com/ssomar/score/utils/CustomColor.java
@@ -0,0 +1,50 @@
+package com.ssomar.score.utils;
+
+import org.bukkit.Color;
+
+public class CustomColor {
+
+	public static Color valueOf(String s) {
+
+		s=s.toUpperCase();
+
+		switch(s) {
+		case "AQUA":
+			return Color.AQUA;
+		case "BLACK":
+			return Color.BLACK;
+		case "BLUE":
+			return Color.BLUE;
+		case "FUCHSIA":
+			return Color.FUCHSIA;
+		case "GRAY":
+			return Color.GRAY;
+		case "GREEN":
+			return Color.GREEN;
+		case "LIME":
+			return Color.LIME;
+		case "MAROON":
+			return Color.MAROON;
+		case "NAVY":
+			return Color.NAVY;
+		case "OLIVE":
+			return Color.OLIVE;
+		case "ORANGE":
+			return Color.ORANGE;
+		case "PURPLE":
+			return Color.PURPLE;
+		case "RED":
+			return Color.RED;
+		case "SILVER":
+			return Color.SILVER;
+		case "TEAL":
+			return Color.TEAL;
+		case "WHITE":
+			return Color.WHITE;
+		case "YELLOW":
+			return Color.YELLOW;
+		default: 
+			return null;
+		}
+	}
+}
