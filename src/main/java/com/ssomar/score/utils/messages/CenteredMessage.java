451c93aa022a82b8b8a1487fc9a0436fe8c4559f
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-08-10 17:12:57 +0200

    Update CenteredMessage.java

--- src/main/java/com/ssomar/score/utils/messages/CenteredMessage.java
+++ src/main/java/com/ssomar/score/utils/messages/CenteredMessage.java
@@ -133,41 +133,8 @@ public enum CenteredMessage{
 
 	private final static int CENTER_PX = 154;
 
-	public static void sendCenteredMessage(Player player, String message){
-		if(message == null || message.equals("")) player.sendMessage("");
-		message = ChatColor.translateAlternateColorCodes('&', message);
-
-		int messagePxSize = 0;
-		boolean previousCode = false;
-		boolean isBold = false;
-
-		for(char c : message.toCharArray()){
-			if(c == '§'){
-				previousCode = true;
-				continue;
-			}else if(previousCode == true){
-				previousCode = false;
-				if(c == 'l' || c == 'L'){
-					isBold = true;
-					continue;
-				}else isBold = false;
-			}else{
-				CenteredMessage dFI = CenteredMessage.getDefaultFontInfo(c);
-				messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
-				messagePxSize++;
-			}
-		}
-
-		int halvedMessageSize = messagePxSize / 2;
-		int toCompensate = CENTER_PX - halvedMessageSize;
-		int spaceLength = CenteredMessage.SPACE.getLength() + 1;
-		int compensated = 0;
-		StringBuilder sb = new StringBuilder();
-		while(compensated < toCompensate){
-			sb.append(" ");
-			compensated += spaceLength;
-		}
-		player.sendMessage(sb.toString() + message);
+	public static void sendCenteredMessage(Player player, String message) {
+		player.sendMessage(message);
 	}
 	
 	public static String convertIntoCenteredMessage(String message){
