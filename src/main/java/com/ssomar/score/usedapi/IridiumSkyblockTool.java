deae58d46b441062a798535d75a50bef52d7cfa6
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-09-23 19:13:26 +0200

    update without IridiumSkyblock

--- src/main/java/com/ssomar/score/usedapi/IridiumSkyblockTool.java
+++ src/main/java/com/ssomar/score/usedapi/IridiumSkyblockTool.java
@@ -5,22 +5,19 @@ import java.util.Optional;
 
 import org.bukkit.entity.Player;
 
-import com.iridium.iridiumskyblock.api.IridiumSkyblockAPI;
-import com.iridium.iridiumskyblock.database.Island;
-import com.iridium.iridiumskyblock.database.User;
 
 public class IridiumSkyblockTool {
 
 	public static boolean playerIsOnHisIsland(Player player) {
 		
-		Optional<Island> islandOpt = IridiumSkyblockAPI.getInstance().getIslandViaLocation(player.getLocation());
+		/*Optional<Island> islandOpt = IridiumSkyblockAPI.getInstance().getIslandViaLocation(player.getLocation());
 		if(islandOpt.isPresent()) {
 			Island island = islandOpt.get();
 			List<User> members = island.getMembers();
 			for(User user : members) {
 				if(player.getUniqueId().equals(user.getUuid())) return true;
 			}
-		}
+		}*/
 
 		return false;
 	}
