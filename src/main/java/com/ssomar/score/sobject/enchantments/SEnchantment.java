8f4388cc65207088c3aaef86c9fc93397a8a89f1
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-10-03 15:14:04 +0200

    Update SEnchantment.java

--- src/main/java/com/ssomar/score/sobject/enchantments/SEnchantment.java
+++ src/main/java/com/ssomar/score/sobject/enchantments/SEnchantment.java
@@ -1,6 +1,7 @@
 package com.ssomar.score.sobject.enchantments;
 
 import com.google.common.base.Charsets;
+import com.ssomar.score.SCore;
 import com.ssomar.score.sobject.SObject;
 import com.ssomar.score.splugin.SPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -39,6 +40,36 @@ public class SEnchantment {
 
 	public void delete(SPlugin sPlugin, SObject sObject){
 		deleteEnchantment(sPlugin, sObject, id);
+	}
+
+	public void saveEnchantments(SPlugin sPlugin, SObject sObject, int level) {
+
+		// TODO not general string
+		if(!new File(sObject.getPath()).exists()) {
+			sPlugin.getPlugin().getLogger().severe(sPlugin.getNameDesign()+" Error can't find the file of the item in the folder items ! ("+sObject.getID()+".yml)");
+			return;
+		}
+		File file = new File(sObject.getPath());
+		FileConfiguration config = (FileConfiguration) YamlConfiguration.loadConfiguration(file);
+
+		config.set("enchantments."+id, null);
+
+			if (!SCore.is1v12()) {
+				config.set("enchantments." + id + ".enchantment", enchantment.getKey().toString().split("minecraft:")[1]);
+			} else config.set("enchantments." +id + ".enchantment", enchantment.getName());
+			config.set("enchantments." + id + ".level", level);
+
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(file), Charsets.UTF_8);
+
+			try {
+				writer.write(config.saveToString());
+			} finally {
+				writer.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void deleteEnchantment(SPlugin sPlugin, SObject sObject, String id) {
