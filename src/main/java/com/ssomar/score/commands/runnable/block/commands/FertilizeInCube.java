01f6e6884f29569d805bd19efb64092f37644ebf
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2021-12-22 17:03:23 +0100

    radius = 0 , fertilize the block

--- src/main/java/com/ssomar/score/commands/runnable/block/commands/FertilizeInCube.java
+++ src/main/java/com/ssomar/score/commands/runnable/block/commands/FertilizeInCube.java
@@ -16,86 +16,93 @@ import com.ssomar.score.utils.ToolsListMaterial;
 import org.jetbrains.annotations.NotNull;
 
 /* FARMINCUBE {radius} {ActiveDrop true or false} */
-public class FertilizeInCube extends BlockCommand{
+public class FertilizeInCube extends BlockCommand {
 
-	@Override
-	public void run(Player p, @NotNull Block block, Material oldMaterial, List<String> args, ActionInfo aInfo) {
-		List<Material> validMaterial = ToolsListMaterial.getInstance().getPlantWithGrowth();
+    @Override
+    public void run(Player p, @NotNull Block block, Material oldMaterial, List<String> args, ActionInfo aInfo) {
+        List<Material> validMaterial = ToolsListMaterial.getInstance().getPlantWithGrowth();
 
-		try {
-			int radius = Integer.parseInt(args.get(0));
-			
-			for(int y = -radius ; y<radius+1 ; y++) {
-				for(int x = -radius ; x<radius+1 ; x++) {
-					for(int z = -radius ; z<radius+1 ; z++) {
+        try {
+            int radius = Integer.parseInt(args.get(0));
+            /* Fertilize only the block selected */
+            if (radius == 0) {
+                Block toDestroy = block;
+                if (!validMaterial.contains(toDestroy.getType())) return;
+                this.grownUpTheBlock(toDestroy, p);
+            } else {
 
-						Block toDestroy = block.getWorld().getBlockAt(block.getX()+x, block.getY()+y, block.getZ()+z);
+                for (int y = -radius; y < radius + 1; y++) {
+                    for (int x = -radius; x < radius + 1; x++) {
+                        for (int z = -radius; z < radius + 1; z++) {
 
-						if(!validMaterial.contains(toDestroy.getType())) continue;
+                            Block toDestroy = block.getWorld().getBlockAt(block.getX() + x, block.getY() + y, block.getZ() + z);
 
-
-						this.grownUpTheBlock(toDestroy,  p);
-					}
-				}
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void grownUpTheBlock(Block toGrownUp, Player p) {
-
-		BlockData data = toGrownUp.getState().getBlockData();
-
-		if(data instanceof Ageable) {
-			Ageable ageable = (Ageable)data;
-			if(ageable.getAge()!=ageable.getMaximumAge()) {
-				ageable.setAge(ageable.getAge()+1);
-			}
-		}
-
-		toGrownUp.setBlockData(data);
-	}
+                            if (!validMaterial.contains(toDestroy.getType())) continue;
 
 
-	@Override
-	public String verify(List<String> args) {
-		String error ="";
+                            this.grownUpTheBlock(toDestroy, p);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		String fertilize= "FERTILIZEINCUBE {range}";
-		if(args.size()>1) error= tooManyArgs+fertilize;
-		else if(args.size()<1) error = notEnoughArgs+fertilize;
-		else if(args.size()==1) { 
-			try {
-				Integer.valueOf(args.get(0));
-			}catch(NumberFormatException e){
-				error = invalidRange+args.get(0)+" for command: "+fertilize;
-			}
-		}
+    public void grownUpTheBlock(Block toGrownUp, Player p) {
 
-		return error;
-	}
-	
-	@Override
-	public List<String> getNames() {
-		List<String> names = new ArrayList<>();
-		names.add("FERTILIZEINCUBE");
-		return names;
-	}
+        BlockData data = toGrownUp.getState().getBlockData();
 
-	@Override
-	public String getTemplate() {
-		return "FERTILIZEINCUBE {radius}";
-	}
+        if (data instanceof Ageable) {
+            Ageable ageable = (Ageable) data;
+            if (ageable.getAge() != ageable.getMaximumAge()) {
+                ageable.setAge(ageable.getAge() + 1);
+            }
+        }
 
-	@Override
-	public ChatColor getColor() {
-		return null;
-	}
+        toGrownUp.setBlockData(data);
+    }
 
-	@Override
-	public ChatColor getExtraColor() {
-		return null;
-	}
+
+    @Override
+    public String verify(List<String> args) {
+        String error = "";
+
+        String fertilize = "FERTILIZEINCUBE {range}";
+        if (args.size() > 1) error = tooManyArgs + fertilize;
+        else if (args.size() < 1) error = notEnoughArgs + fertilize;
+        else if (args.size() == 1) {
+            try {
+                Integer.valueOf(args.get(0));
+            } catch (NumberFormatException e) {
+                error = invalidRange + args.get(0) + " for command: " + fertilize;
+            }
+        }
+
+        return error;
+    }
+
+    @Override
+    public List<String> getNames() {
+        List<String> names = new ArrayList<>();
+        names.add("FERTILIZEINCUBE");
+        return names;
+    }
+
+    @Override
+    public String getTemplate() {
+        return "FERTILIZEINCUBE {radius}";
+    }
+
+    @Override
+    public ChatColor getColor() {
+        return null;
+    }
+
+    @Override
+    public ChatColor getExtraColor() {
+        return null;
+    }
 
 }
