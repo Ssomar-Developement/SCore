79660dbc87256ab95b8c13bbfffac7737d72781d
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-05-09 14:35:42 +0200

    Update ExecutableItemInterface.java

--- src/main/java/com/ssomar/score/api/executableitems/config/ExecutableItemInterface.java
+++ src/main/java/com/ssomar/score/api/executableitems/config/ExecutableItemInterface.java
@@ -9,27 +9,34 @@ import java.util.Optional;
 
 public interface ExecutableItemInterface extends SObject {
 
-    /** To place at the end of your itemBuilder , it adds infos for item to be recognized as an ExecutableItem
+    /**
+     * To place at the end of your itemBuilder , it adds infos for item to be recognized as an ExecutableItem
      * It will take the lore / name of the ExecutableItems and Override yours (But it doesn't override the customModeldata tag)
-     * @param item The item to add the ExecutableItem infos to
+     *
+     * @param item    The item to add the ExecutableItem infos to
      * @param creator The optional creator of the ExecutableItem
-     * */
+     */
     ItemStack addExecutableItemInfos(ItemStack item, Optional<Player> creator);
 
     /**
-     * @param player The player to whom you want to check the possession of the permission
+     * @param player    The player to whom you want to check the possession of the permission
      * @param showError true if you want to show an error message to the player if he doesn't have the permission
-     * @return The name of the ExecutableItem */
-    boolean hasItemPerm(@NotNull Player player,  boolean showError);
-
+     * @return The name of the ExecutableItem
+     */
+    boolean hasItemPerm(@NotNull Player player, boolean showError);
 
 
     /**
      * Build the ExecutableItem
-     * @param amount The amount of the ExecutableItem
-     * @param usage The optional custom usage of the ExecutableItem, otherwise it will use the default one
+     *
+     * @param amount  The amount of the ExecutableItem
+     * @param usage   The optional custom usage of the ExecutableItem, otherwise it will use the default one
      * @param creator The optional creator of the ExecutableItem
      * @return The ExecutableItem
      */
-     ItemStack buildItem(int amount, Optional<Integer> usage, Optional<Player> creator);
+    ItemStack buildItem(int amount, Optional<Integer> usage, Optional<Player> creator);
+
+    /**
+     * @return true If the item has the feature to keep the EI on death, false otherwise **/
+    boolean hasKeepItemOnDeath();
 }
