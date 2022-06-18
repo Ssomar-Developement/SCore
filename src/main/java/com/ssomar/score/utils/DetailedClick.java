766a421c9438c312cdf7bca498367447e4afb5eb
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-06-18 15:16:16 +0200

    Update DetailedClick.java

--- src/main/java/com/ssomar/score/utils/DetailedClick.java
+++ src/main/java/com/ssomar/score/utils/DetailedClick.java
@@ -4,4 +4,22 @@ public enum DetailedClick {
     LEFT,
     RIGHT,
     RIGHT_OR_LEFT;
+
+
+    public static boolean isDetailedClick(String s){
+        return s.equalsIgnoreCase("left") || s.equalsIgnoreCase("right") || s.equalsIgnoreCase("right_or_left");
+    }
+
+    public static DetailedClick getDetailedClick(String s){
+        if(s.equalsIgnoreCase("left")){
+            return LEFT;
+        }
+        if(s.equalsIgnoreCase("right")){
+            return RIGHT;
+        }
+        if(s.equalsIgnoreCase("right_or_left")){
+            return RIGHT_OR_LEFT;
+        }
+        return null;
+    }
 }
