9f5a6dd238dc583507858667d192f6e570c02030
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-07-27 18:02:50 +0200

    Update ListWorldFeature.java

--- src/main/java/com/ssomar/score/features/types/list/ListWorldFeature.java
+++ src/main/java/com/ssomar/score/features/types/list/ListWorldFeature.java
@@ -2,44 +2,33 @@ package com.ssomar.score.features.types.list;
 
 import com.ssomar.score.editor.NewGUIManager;
 import com.ssomar.score.editor.Suggestion;
-import com.ssomar.score.features.FeatureAbstract;
 import com.ssomar.score.features.FeatureParentInterface;
-import com.ssomar.score.features.FeatureRequireSubTextEditorInEditor;
-import com.ssomar.score.features.FeatureReturnCheckPremium;
 import com.ssomar.score.menu.EditorCreator;
 import com.ssomar.score.menu.GUI;
-import com.ssomar.score.splugin.SPlugin;
 import com.ssomar.score.usedapi.MultiverseAPI;
 import com.ssomar.score.utils.StringConverter;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import java.util.*;
 
 @Getter
 @Setter
-public class ListWorldFeature extends FeatureAbstract<List<String>, ListWorldFeature> implements FeatureRequireSubTextEditorInEditor {
+public class ListWorldFeature extends ListFeatureAbstract<String, ListWorldFeature> {
 
-    private List<String> value;
-    private List<String> defaultValue;
-    private boolean notSaveIfEqualsToDefaultValue;
 
     public ListWorldFeature(FeatureParentInterface parent, String name, List<String> defaultValue, String editorName, String[] editorDescription, Material editorMaterial, boolean requirePremium, boolean notSaveIfEqualsToDefaultValue) {
-        super(parent, name, editorName, editorDescription, editorMaterial, requirePremium);
-        this.defaultValue = defaultValue;
-        this.notSaveIfEqualsToDefaultValue = notSaveIfEqualsToDefaultValue;
+        super(parent, name, "List of Worlds", editorName, editorDescription, editorMaterial, defaultValue, requirePremium, notSaveIfEqualsToDefaultValue);
         reset();
     }
 
     @Override
-    public List<String> load(SPlugin plugin, ConfigurationSection config, boolean isPremiumLoading) {
-        List<String> errors = new ArrayList<>();
-        value = new ArrayList<>();
-        for (String s : config.getStringList(this.getName())) {
+    public List<String> loadValue(List<String> entries, List<String> errors) {
+        List<String> value = new ArrayList<>();
+        for (String s : entries) {
             s = StringConverter.decoloredString(s);
             World w = MultiverseAPI.getWorld(s);
             if (w != null) {
@@ -47,53 +36,15 @@ public class ListWorldFeature extends FeatureAbstract<List<String>, ListWorldFea
             } else
                 errors.add("&cERROR, Couldn't load the World value of " + this.getName() + " from config, value: " + s + " &7&o" + getParent().getParentInfo());
         }
-        FeatureReturnCheckPremium<List<String>> checkPremium = checkPremium("List of Worlds", value, Optional.of(defaultValue), isPremiumLoading);
-        if (checkPremium.isHasError()) value = checkPremium.getNewValue();
-        return errors;
-    }
-
-    @Override
-    public void save(ConfigurationSection config) {
-        if (notSaveIfEqualsToDefaultValue) {
-            if (defaultValue.containsAll(value)) {
-                config.set(this.getName(), null);
-                return;
-            }
-        }
-        config.set(this.getName(), value);
-    }
-
-    @Override
-    public List<String> getValue() {
         return value;
     }
 
-    @Override
-    public ListWorldFeature initItemParentEditor(GUI gui, int slot) {
-        String[] finalDescription = new String[getEditorDescription().length + 2];
-        System.arraycopy(getEditorDescription(), 0, finalDescription, 0, getEditorDescription().length);
-        finalDescription[finalDescription.length - 2] = GUI.CLICK_HERE_TO_CHANGE;
-        finalDescription[finalDescription.length - 1] = "&7actually: ";
-
-        gui.createItem(getEditorMaterial(), 1, slot, GUI.TITLE_COLOR + getEditorName(), false, false, finalDescription);
-        return this;
-    }
-
-    @Override
-    public void updateItemParentEditor(GUI gui) {
-        gui.updateConditionList(getEditorName(), getValue(), "&cEMPTY");
-    }
 
     @Override
     public ListWorldFeature clone(FeatureParentInterface newParent) {
         ListWorldFeature clone = new ListWorldFeature(newParent, this.getName(), getDefaultValue(), getEditorName(), getEditorDescription(), getEditorMaterial(), isRequirePremium(), isNotSaveIfEqualsToDefaultValue());
         clone.setValue(getValue());
         return clone;
-    }
-
-    @Override
-    public void reset() {
-        this.value = new ArrayList<>(defaultValue);
     }
 
     @Override
@@ -108,7 +59,7 @@ public class ListWorldFeature extends FeatureAbstract<List<String>, ListWorldFea
 
     @Override
     public List<String> getCurrentValues() {
-        return value;
+        return getValue();
     }
 
     @Override
@@ -127,10 +78,11 @@ public class ListWorldFeature extends FeatureAbstract<List<String>, ListWorldFea
 
     @Override
     public void finishEditInSubEditor(Player editor, NewGUIManager manager) {
-        value = (List<String>) manager.currentWriting.get(editor);
-        for (int i = 0; i < value.size(); i++) {
-            value.set(i, StringConverter.decoloredString(value.get(i)));
+        List<String> value = new ArrayList<>();
+        for (String s : (List<String>) manager.currentWriting.get(editor)) {
+            value.add(StringConverter.decoloredString(s));
         }
+        setValue(value);
         manager.requestWriting.remove(editor);
         manager.activeTextEditor.remove(editor);
         updateItemParentEditor((GUI) manager.getCache().get(editor));
