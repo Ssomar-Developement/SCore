f936fbd26601d1e43973542f29945c4e2e276cec
Ssomar <46328697+ssomar1607@users.noreply.github.com>
2022-09-24 19:14:07 +0200

    Update

--- src/main/java/com/ssomar/score/events/TESTEVENT_TODELETE.java
+++ src/main/java/com/ssomar/score/events/TESTEVENT_TODELETE.java
@@ -8,12 +8,12 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class TESTEVENT_TODELETE implements Listener {
 
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    /*@EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntityEvent(PlayerInteractEvent e) {
 
         e.setCancelled(true);
         e.setUseItemInHand(org.bukkit.event.Event.Result.DENY);
         e.setUseInteractedBlock(org.bukkit.event.Event.Result.DENY);
 
-    }
+    }*/
 }
