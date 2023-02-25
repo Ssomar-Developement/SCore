9bc38ebaf44c0665682371f031148d701f65e107
Vayk <benjaminalejandro.agustin@gmail.com>
2023-02-25 13:33:23 -0300

    lore commands

---
+++ src/main/java/com/ssomar/score/commands/runnable/player/commands/Setlore.java
@@ -0,0 +1,87 @@
+package com.ssomar.score.commands.runnable.player.commands;
+
+import com.ssomar.score.commands.runnable.ActionInfo;
+import com.ssomar.score.commands.runnable.ArgumentChecker;
+import com.ssomar.score.commands.runnable.player.PlayerCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/* SETLORE {slot} {line} {text} */
+public class Setlore extends PlayerCommand {
+
+    @Override
+    public void run(Player p, Player receiver, List<String> args, ActionInfo aInfo) {
+
+        ItemStack item;
+        ItemMeta itemmeta;
+
+        try {
+            item = receiver.getInventory().getItem(Integer.valueOf(args.get(0)));
+            itemmeta = item.getItemMeta();
+
+            StringBuilder build = new StringBuilder();
+            for (int i = 2; i < args.size(); i++) {
+                build.append(args.get(i) + " ");
+            }
+
+            List<String> list = itemmeta.getLore();
+
+            if (list == null) return;
+            if (list.size() < Integer.valueOf(args.get(1))) return;
+
+            Integer index = Integer.valueOf(args.get(1));
+            if(index > 0) index += -1;
+            list.set(index, ChatColor.translateAlternateColorCodes('&', build.toString()));
+
+            itemmeta.setLore(list);
+            item.setItemMeta(itemmeta);
+
+            receiver.getInventory().setItem(Integer.valueOf(args.get(0)),item);
+
+        } catch (NullPointerException e) {
+            return;
+        }
+
+    }
+
+    @Override
+    public Optional<String> verify(List<String> args, boolean isFinalVerification) {
+        if (args.size() < 3) return Optional.of(notEnoughArgs + getTemplate());
+
+        ArgumentChecker ac = checkInteger(args.get(0), isFinalVerification, getTemplate());
+        if (!ac.isValid()) return Optional.of(ac.getError());
+
+        ArgumentChecker ac2 = checkInteger(args.get(1), isFinalVerification, getTemplate());
+        if (!ac2.isValid()) return Optional.of(ac2.getError());
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<String> getNames() {
+        List<String> names = new ArrayList<>();
+        names.add("SETLORE");
+        return names;
+    }
+
+    @Override
+    public String getTemplate() {
+        return "SETLORE {slot} {line} {text dont need brackets}";
+    }
+
+    @Override
+    public ChatColor getColor() {
+        return null;
+    }
+
+    @Override
+    public ChatColor getExtraColor() {
+        return null;
+    }
+}
