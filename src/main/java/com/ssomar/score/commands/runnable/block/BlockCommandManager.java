0b59179632ff317b8d2439588ce19f89913937ca
Vayk <benjaminalejandro.agustin@gmail.com>
2023-03-02 20:51:37 -0300

    oxygen and offsetmineincube

--- src/main/java/com/ssomar/score/commands/runnable/block/BlockCommandManager.java
+++ src/main/java/com/ssomar/score/commands/runnable/block/BlockCommandManager.java
@@ -31,6 +31,7 @@ public class BlockCommandManager extends CommandManager<BlockCommand> {
         references.add(new DropExecutableItem());
         references.add(new DropExecutableBlock());
         references.add(new MineInCube());
+        references.add(new OffsetMineInCube());
         references.add(new RemoveBlock());
         references.add(new Around());
         references.add(new MobAround());
