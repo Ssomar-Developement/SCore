948b4ab00d43de3454fad9fa0b3817146ff4a0dc
Marceau LEYMONERIE <marceau1607@gmail.com>
2023-03-24 16:47:03 +0100

    Update GlowDropManager.java

--- src/main/java/com/ssomar/score/features/custom/drop/glowdrop/GlowDropManager.java
+++ src/main/java/com/ssomar/score/features/custom/drop/glowdrop/GlowDropManager.java
@@ -1,6 +1,5 @@
 package com.ssomar.score.features.custom.drop.glowdrop;
 
-import com.ssomar.executableitems.ExecutableItems;
 import com.ssomar.score.SCore;
 import com.ssomar.score.SsomarDev;
 import org.bukkit.ChatColor;
@@ -10,18 +9,24 @@ import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 public class GlowDropManager {
 
     private static GlowDropManager instance;
     private final HashMap<ChatColor, Team> teams;
 
+    private Map<UUID, ChatColor> links = new HashMap<>();
+
 
     public GlowDropManager() {
         instance = this;
         teams = new HashMap<>();
+        links = new HashMap<>();
         if (!SCore.is1v11Less()) {
-            ScoreboardManager scoreboardManager = ExecutableItems.plugin.getServer().getScoreboardManager();
+            ScoreboardManager scoreboardManager = SCore.plugin.getServer().getScoreboardManager();
             for (ChatColor color : ChatColor.values()) {
                 Team team;
                 try {
@@ -44,22 +49,41 @@ public class GlowDropManager {
         return instance;
     }
 
+    public Optional<String> onRequestPlaceholder(OfflinePlayer player, String params) {
+        if (params.startsWith("cmd-glow")) {
+            if (links.containsKey(player.getUniqueId())) {
+                return Optional.of("&" + links.get(player.getUniqueId()).getChar());
+            }
+            return Optional.of("");
+        }
+        return Optional.empty();
+    }
+
     public void addGlow(Entity entity, ChatColor color) {
-        SsomarDev.testMsg("addGlow : "+entity.getType()+ " "+color.name(), false);
+        SsomarDev.testMsg("addGlow : " + entity.getType() + " " + color.name(), false);
+        links.put(entity.getUniqueId(), color);
+        /* if(SCore.hasTAB && entity instanceof Player){
+            TabPlayer tabPlayer = TabAPI.getInstance().getPlayer(entity.getUniqueId());
+            TabAPI.getInstance().getTeamManager().updateTeamData(tabPlayer);
+        }*/
         if (teams.containsKey(color)) {
             Team team = teams.get(color);
             team.addEntry(entity.getUniqueId().toString());
-            if(entity instanceof OfflinePlayer) team.addEntry(((OfflinePlayer) entity).getName());
+            if (entity instanceof OfflinePlayer) team.addEntry(((OfflinePlayer) entity).getName());
             entity.setGlowing(true);
-        }
-        else SsomarDev.testMsg("color not found > "+color.name(), false);
+        } else SsomarDev.testMsg("color not found > " + color.name(), false);
     }
 
     public void removeGlow(Entity entity, ChatColor color) {
+        links.remove(entity.getUniqueId());
+        /* if(SCore.hasTAB && entity instanceof Player){
+            TabPlayer tabPlayer = TabAPI.getInstance().getPlayer(entity.getUniqueId());
+            TabAPI.getInstance().getTeamManager().resumeTeamHandling(tabPlayer);
+        }*/
         if (teams.containsKey(color)) {
             Team team = teams.get(color);
             team.removeEntry(entity.getUniqueId().toString());
-            if(entity instanceof OfflinePlayer) team.removePlayer((OfflinePlayer) entity);
+            if (entity instanceof OfflinePlayer) team.removePlayer((OfflinePlayer) entity);
             entity.setGlowing(false);
         }
     }
