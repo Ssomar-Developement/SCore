2ed75957745be042170941b210db6271bf2fb673
monckey100 <lf2owner@hotmail.com>
2024-12-06 17:24:16 -0800

    fixed so it uses itemmeta

--- src/main/java/com/ssomar/score/commands/runnable/player/commands/SetItemTooltipStyle.java
+++ src/main/java/com/ssomar/score/commands/runnable/player/commands/SetItemTooltipStyle.java
@@ -5,8 +5,11 @@ import com.ssomar.score.commands.runnable.SCommandToExec;
 import com.ssomar.score.commands.runnable.player.PlayerCommand;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,24 +19,29 @@ public class SetItemTooltipStyle extends PlayerCommand {
     public SetItemTooltipStyle() {
         CommandSetting slot = new CommandSetting("slot", 0, Integer.class, -1);
         slot.setSlot(true);
-        CommandSetting material = new CommandSetting("tooltipModel", 1, String.class, "namespace:id");
+        CommandSetting String = new CommandSetting("tooltipModel", 1, String.class, "namespace:id");
         List<CommandSetting> settings = getSettings();
         settings.add(slot);
-        settings.add(material);
+        settings.add(String);
         setNewSettingsMode(true);
     }
 
     @Override
     public void run(Player p, Player receiver, SCommandToExec sCommandToExec) {
         ItemStack item;
-        Material material = (Material) sCommandToExec.getSettingValue("tooltipModel");
+        NamespacedKey material = NamespacedKey.fromString((String) sCommandToExec.getSettingValue("tooltipModel"));
         int slot = (int) sCommandToExec.getSettingValue("slot");
         if (slot == -1) item = receiver.getInventory().getItemInMainHand();
         else item = receiver.getInventory().getItem(slot);
 
         if (item == null || item.getType() == Material.AIR) return;
+        if(!item.hasItemMeta()){
+            item.setItemMeta(new ItemStack(item.getType()).getItemMeta());
+        }
 
-        item.setType(material);
+        ItemMeta itemmeta = item.getItemMeta();
+
+        itemmeta.setTooltipStyle(material);
 
     }
 
