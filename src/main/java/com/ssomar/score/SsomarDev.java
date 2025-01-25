422f2f560d01fe6b2bea61500db4650d61eda3e7
Marceau LEYMONERIE <marceau1607@gmail.com>
2025-01-26 00:10:58 +0100

    5.25.1.25

--- src/main/java/com/ssomar/score/SsomarDev.java
+++ src/main/java/com/ssomar/score/SsomarDev.java
@@ -5,7 +5,7 @@ import org.bukkit.Bukkit;
 public class SsomarDev {
 
     public static void testMsg(String message, boolean isActiveDebug) {
-        if (isActiveDebug /* replace */) {
+        if (false && false && isActiveDebug /* replace */) {
             try {
                 Bukkit.getPlayer("Ssomarr").sendMessage(message);
             } catch (Exception ignored) {}
