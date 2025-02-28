6111c8e5f4b7d76ddd42f3325f5d1ddca609aec7
Marceau LEYMONERIE <marceau1607@gmail.com>
2025-02-28 01:42:15 +0100

    Update

--- src/main/java/com/ssomar/score/utils/logging/Utils.java
+++ src/main/java/com/ssomar/score/utils/logging/Utils.java
@@ -3,18 +3,26 @@ package com.ssomar.score.utils.logging;
 import com.ssomar.score.SCore;
 import com.ssomar.score.splugin.SPlugin;
 import com.ssomar.score.utils.strings.StringConverter;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 public class Utils {
 
     public static void sendConsoleMsg(String msg) {
-       sendConsoleMsg(SCore.plugin, msg);
+        // SCore shading
+        if(SCore.plugin == null) sendConsoleMsg(Bukkit.getPluginManager().getPlugins()[0], msg);
+        else sendConsoleMsg((SPlugin) SCore.plugin, msg);
     }
 
     public static void sendConsoleMsg(SPlugin sPlugin, String msg) {
+        sendConsoleMsg(sPlugin.getPlugin(), msg);
+    }
+
+    public static void sendConsoleMsg(Plugin plugin, String msg) {
         if (SCore.is1v16Plus())
-            sPlugin.getPlugin().getServer().getConsoleSender().sendMessage(StringConverter.coloredString(msg));
+            plugin.getServer().getConsoleSender().sendMessage(StringConverter.coloredString(msg));
         else
-            sPlugin.getPlugin().getServer().getConsoleSender().sendMessage(StringConverter.decoloredString(msg));
+            plugin.getServer().getConsoleSender().sendMessage(StringConverter.decoloredString(msg));
     }
 
     public static void sendConsoleFlatMsg(SPlugin sPlugin, String msg) {
