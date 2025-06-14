38aae1c553feab40b06816a9d2fe8fb79fe2d94b
Marceau LEYMONERIE <marceau1607@gmail.com>
2025-06-14 23:44:57 +0200

    5.25.6.14

--- src/main/java/com/ssomar/score/commands/runnable/mixed_player_entity/commands/AddTemporaryAttribute.java
+++ src/main/java/com/ssomar/score/commands/runnable/mixed_player_entity/commands/AddTemporaryAttribute.java
@@ -1,21 +1,20 @@
 package com.ssomar.score.commands.runnable.mixed_player_entity.commands;
-import com.ssomar.score.utils.backward_compatibility.AttributeUtils;
 
 import com.ssomar.score.SCore;
 import com.ssomar.score.commands.runnable.SCommandToExec;
 import com.ssomar.score.commands.runnable.mixed_player_entity.MixedCommand;
+import com.ssomar.score.utils.backward_compatibility.AttributeUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import javax.xml.stream.events.Namespace;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -105,12 +104,9 @@ public class AddTemporaryAttribute extends MixedCommand  {
 
         // the entity arg in the method arguments refer to the target of the command and the player arg represents the caster.
         // if the target is a Player, cast the entity as a player.
-        if (entity instanceof Player) {
-            Player playerEntity = (Player) entity;
-            attrInstance = playerEntity.getAttribute(AttributeUtils.getAttribute(args.get(0).toUpperCase()));
-        } else {
+        if (entity instanceof LivingEntity) {
             LivingEntity livingEntity = (LivingEntity) entity;
-            attrInstance = livingEntity.getAttribute((AttributeUtils.getAttribute(args.get(0).toUpperCase())));
+            attrInstance = livingEntity.getAttribute((attrCheck));
         }
 
         // make a randomized key to allow spamming of ADD_TEMPORARY_ATTRIBUTE
