e613e9630a1854b47eecc8bd70f229e5c51beea5
Josef Miko Urquico <josef.urquico@gmail.com>
2025-06-21 11:55:58 +0800

    update logic issues when it comes to edge cases
    - properly wrapped the int array inside the try catch block
    - fixed a mistake in indicating target indexes

--- src/main/java/com/ssomar/score/utils/emums/CustomColor.java
+++ src/main/java/com/ssomar/score/utils/emums/CustomColor.java
@@ -54,11 +54,19 @@ public class CustomColor {
             // sample usage: "RGB-94-84-214"
             // go to https://www.tydac.ch/color/ and pick a color and get the RGB values
 
-            String[] customRGB = s.split("-");
-            if (customRGB.length != 4) return null; // to deny invalid entries
+            String[] parts = s.split("-");
+
+            // input length after split must be 4 to make the input only work by following this format.
+            if (parts.length != 4) return null;
+
             try {
-                if (Integer.parseInt(customRGB[1]) > 255 || Integer.parseInt(customRGB[2]) > 255 || Integer.parseInt(customRGB[3]) > 255) return null; // plugin freaks out if a value higher than 255 got involved
-                return Color.fromRGB(Integer.parseInt(customRGB[1]), Integer.parseInt(customRGB[2]), Integer.parseInt(customRGB[3]));
+                int[] customRGB = new int[]{
+                        Integer.parseInt(parts[1]),
+                        Integer.parseInt(parts[2]),
+                        Integer.parseInt(parts[3])
+                };
+                if (customRGB[0] > 255 || customRGB[1] > 255 || customRGB[2] > 255) return null; // plugin freaks out if a value higher than 255 got involved
+                else return Color.fromRGB(customRGB[0], customRGB[1], customRGB[2]);
             } catch (Exception e) {
                 return null;
             }
