f39eda4e82137012a224416827ed4daa3553ceb3
Josef Miko Urquico <josef.urquico@gmail.com>
2025-11-21 10:49:38 +0800

    Added a new argument for DROPEXECUTABLEITEM
    - Updated the argument system to the latest version
    - Added "owner" argument where you get to define the owner of the dropped item if the item's store item info is enabled

--- src/main/java/com/ssomar/score/commands/runnable/block/commands/DropExecutableItem.java
+++ src/main/java/com/ssomar/score/commands/runnable/block/commands/DropExecutableItem.java
@@ -3,8 +3,10 @@ package com.ssomar.score.commands.runnable.block.commands;
 import com.ssomar.score.SCore;
 import com.ssomar.score.api.executableitems.ExecutableItemsAPI;
 import com.ssomar.score.api.executableitems.config.ExecutableItemInterface;
+import com.ssomar.score.commands.runnable.CommandSetting;
 import com.ssomar.score.commands.runnable.SCommandToExec;
 import com.ssomar.score.commands.runnable.block.BlockCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -13,19 +15,43 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class DropExecutableItem extends BlockCommand {
 
+    public DropExecutableItem() {
+        CommandSetting id = new CommandSetting("id", 0, String.class, "null");
+        CommandSetting amount = new CommandSetting("amount", 1, Integer.class, 1);
+        CommandSetting owner = new CommandSetting("owner", 2, String.class, null);
+        List<CommandSetting> settings = getSettings();
+        settings.add(id);
+        settings.add(amount);
+        settings.add(owner);
+        setNewSettingsMode(true);
+    }
+
     @Override
     public void run(Player p, @NotNull Block block, SCommandToExec sCommandToExec) {
-        List<String> args = sCommandToExec.getOtherArgs();
-        if (SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(args.get(0))) {
-            int amount = Double.valueOf(args.get(1)).intValue();
+        String id = (String) sCommandToExec.getSettingValue("id");
+        int amount = (int) sCommandToExec.getSettingValue("amount");
+        String owner = (String) sCommandToExec.getSettingValue("owner");
+
+        if (!(SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(id))) {
+            SCore.plugin.getLogger().info(ChatColor.RED+"Invalid ID was provided for a DROPEXECUTABLEITEM command. Please double check your DROPEXECUTABLEITEM commands.");
+            return;
+        }
+
+        // Attempt to get player instance of the provided uuid. If null, it will
+        // default to the person who executed the cmd.
+        Player playerOwner = Bukkit.getPlayer(UUID.fromString(owner));
+        if (playerOwner == null) playerOwner = p;
+
+        if (SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(id)) {
             if (amount > 0) {
-                Optional<ExecutableItemInterface> eiOpt = ExecutableItemsAPI.getExecutableItemsManager().getExecutableItem(args.get(0));
+                Optional<ExecutableItemInterface> eiOpt = ExecutableItemsAPI.getExecutableItemsManager().getExecutableItem(id);
                 if (eiOpt.isPresent()) {
                     ExecutableItemInterface ei = eiOpt.get();
-                    block.getWorld().dropItem(block.getLocation(), ei.buildItem(amount, Optional.empty(), Optional.ofNullable(p)));
+                    block.getWorld().dropItem(block.getLocation(), ei.buildItem(amount, Optional.empty(), Optional.ofNullable(playerOwner)));
                 }
             }
         }
@@ -33,7 +59,7 @@ public class DropExecutableItem extends BlockCommand {
 
     @Override
     public Optional<String> verify(List<String> args, boolean isFinalVerification) {
-        return com.ssomar.score.commands.runnable.entity.commands.DropExecutableItem.staticVerif(args, isFinalVerification, getTemplate());
+        return Optional.empty();
     }
 
     @Override
--- src/main/java/com/ssomar/score/commands/runnable/entity/commands/DropExecutableItem.java
+++ src/main/java/com/ssomar/score/commands/runnable/entity/commands/DropExecutableItem.java
@@ -4,8 +4,10 @@ import com.ssomar.score.SCore;
 import com.ssomar.score.api.executableitems.ExecutableItemsAPI;
 import com.ssomar.score.api.executableitems.config.ExecutableItemInterface;
 import com.ssomar.score.commands.runnable.ArgumentChecker;
+import com.ssomar.score.commands.runnable.CommandSetting;
 import com.ssomar.score.commands.runnable.SCommandToExec;
 import com.ssomar.score.commands.runnable.entity.EntityCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -13,26 +15,49 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 
 public class DropExecutableItem extends EntityCommand {
 
     public static final Boolean DEBUG = false;
 
-    //
+    public DropExecutableItem() {
+        CommandSetting id = new CommandSetting("id", 0, String.class, "null");
+        CommandSetting amount = new CommandSetting("amount", 1, Integer.class, 1);
+        CommandSetting owner = new CommandSetting("owner", 2, String.class, null);
+        List<CommandSetting> settings = getSettings();
+        settings.add(id);
+        settings.add(amount);
+        settings.add(owner);
+        setNewSettingsMode(true);
+    }
+
     @Override
     public void run(Player p, Entity entity, SCommandToExec sCommandToExec) {
-        List<String> args = sCommandToExec.getOtherArgs();
+        String id = (String) sCommandToExec.getSettingValue("id");
+        int amount = (int) sCommandToExec.getSettingValue("amount");
+        String owner = (String) sCommandToExec.getSettingValue("owner");
+
+        if (!(SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(id))) {
+            SCore.plugin.getLogger().info(ChatColor.RED+"Invalid ID was provided for a DROPEXECUTABLEITEM command. Please double check your DROPEXECUTABLEITEM commands.");
+            return;
+        }
+
+        // Attempt to get player instance of the provided uuid. If n    ull, it will
+        // default to the person who executed the cmd.
+        Player playerOwner = Bukkit.getPlayer(UUID.fromString(owner));
+        if (playerOwner == null) playerOwner = p;
+
         //SsomarDev.testMsg("DropExecutableItem.run()", DEBUG);
-        if (SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(args.get(0))) {
+        if (SCore.hasExecutableItems && ExecutableItemsAPI.getExecutableItemsManager().isValidID(id)) {
             //SsomarDev.testMsg("DropExecutableItem.run() - hasExecutableItems", DEBUG);
-            int amount = Double.valueOf(args.get(1)).intValue();
             if (amount > 0) {
                 //SsomarDev.testMsg("DropExecutableItem.run() - amount > 0", DEBUG);
-                Optional<ExecutableItemInterface> eiOpt = ExecutableItemsAPI.getExecutableItemsManager().getExecutableItem(args.get(0));
+                Optional<ExecutableItemInterface> eiOpt = ExecutableItemsAPI.getExecutableItemsManager().getExecutableItem(id);
                 if (eiOpt.isPresent()) {
                     //SsomarDev.testMsg(">> loc: " + entity.getLocation());
-                    entity.getWorld().dropItem(entity.getLocation(), eiOpt.get().buildItem(amount, Optional.empty(), Optional.ofNullable(p)));
+                    entity.getWorld().dropItem(entity.getLocation(), eiOpt.get().buildItem(amount, Optional.empty(), Optional.ofNullable(playerOwner)));
                 }
             }
         }
@@ -41,20 +66,9 @@ public class DropExecutableItem extends EntityCommand {
 
     @Override
     public Optional<String> verify(List<String> args, boolean isFinalVerification) {
-       return staticVerif(args, isFinalVerification, getTemplate());
-    }
-
-    public static Optional<String> staticVerif(List<String> args, boolean isFinalVerification, String template) {
-        if (args.size() < 2) return Optional.of(notEnoughArgs + template);
-
-        ArgumentChecker ac = checkExecutableItemID(args.get(0), isFinalVerification, template);
-        if (!ac.isValid()) return Optional.of(ac.getError());
-
-        ArgumentChecker ac2 = checkDouble(args.get(1), isFinalVerification, template);
-        if (!ac2.isValid()) return Optional.of(ac2.getError());
-
         return Optional.empty();
     }
+
 
     @Override
     public List<String> getNames() {
